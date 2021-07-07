ab48c85532b63982d5765ba1a71327a0daf41da3
William Li <email@williamli.dev>
2021-07-07 17:15:00 +0800

    use router.replace to handle redirect /en -> / when rendered

--- next.config.js
+++ next.config.js
@@ -1,17 +1,17 @@
 module.exports = {
-  trailingSlash: true,
+  // trailingSlash: true,
   i18n: {
     locales: ['en', 'fr', 'de'],
     defaultLocale: 'en'
   },
   async redirects() {
     return [
-      {
-        source: '/en/',
-        destination: '/',
-        permanent: false,
-        locale: false
-      },
+      // {
+      //   source: '/en/',
+      //   destination: '/',
+      //   permanent: false,
+      //   locale: false
+      // },
       // {
       //   source: '/en/:path*',
       //   destination: '/:path*',
--- pages/index.js
+++ pages/index.js
@@ -10,31 +10,36 @@ export default function Home() {
     'de': 'Hallo'
   }
   console.log({router})
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          HELLO in {router.locale} is <br/>"{hello[router.locale]}"
-        </h1>
+  if (router.locale === 'en') {
+    router.replace('/')
+  } else {
+    return (
+      <div className={styles.container}>
+        <Head>
+          <title>Create Next App</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-        
-      </main>
+        <main className={styles.main}>
+          <h1 className={styles.title}>
+            HELLO in {router.locale} is <br/>"{hello[router.locale]}"
+          </h1>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+          
+        </main>
+
+        <footer className={styles.footer}>
+          <a
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Powered by{' '}
+            <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+          </a>
+        </footer>
+      </div>
+    )
+  }
 }
